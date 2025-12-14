26294b5210d6e799fd53c905d949889460e7f223
Hussein-Gharib <gharibhu56@gmail.com>
2025-12-14 15:12:35 +0100

    login and register setup

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,12 +2,14 @@ import { Routes, Route } from 'react-router-dom';
 import ProductsPage from './pages/ProductsPage';
 import LoginPage from './pages/LoginPage';
 import CartPage from './pages/CartPage';
+import RegisterPage from './pages/RegisterPage'; 
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<ProductsPage />} />
       <Route path="/login" element={<LoginPage />} />
+      <Route path="/register" element={<RegisterPage />} /> 
       <Route path="/cart" element={<CartPage />} />
     </Routes>
   );
